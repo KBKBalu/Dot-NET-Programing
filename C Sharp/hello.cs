fa6778e29087b0153b397c85163434f825f5d599
Balakrishna <iitbalu1993@gmail.com>
2017-07-11 14:57:05 +0530

    Sample Program of C#

---
+++ C Sharp/hello.cs
@@ -0,0 +1,20 @@
+
+// Sample Program of C#
+using System;
+namespace HelloWorldApplication
+{
+   class HelloWorld
+   {
+      static void Main(string[] args)
+      {
+            int z = 24;
+         /* my first program in C# */
+         Console.WriteLine("Hello World");
+         Console.WriteLine("Hello Balu!!!");
+         System.Console.WriteLine(z);
+         System.Console.WriteLine(z.GetType());
+         Console.ReadKey();
+         
+      }
+   }
+}
