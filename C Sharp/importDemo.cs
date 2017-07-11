7eefdf0ec53353db13a5ee822e102928a6857003
Balakrishna <iitbalu1993@gmail.com>
2017-07-11 14:58:37 +0530

    Importing NameSpace in C#

---
+++ C Sharp/importDemo.cs
@@ -0,0 +1,16 @@
+
+//Importing NameSpace
+using System;
+namespace importDemoApp
+{
+	class importDemo
+	{
+	   static void Main()
+	  {
+		Console.WriteLine("importing a NameSpace");
+		Console.ReadKey();
+
+	  }
+    }
+}
+
