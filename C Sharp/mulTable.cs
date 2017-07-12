60c96c43e402021f781e11058796c8d446064996
Balakrishna <iitbalu1993@gmail.com>
2017-07-12 15:36:45 +0530

    multiplication Table code by using C#

---
+++ C Sharp/mulTable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mulTable
+{
+    class mulTableApplication
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter an Integer value:");
+            int x = int.Parse(Console.ReadLine());
+            if (x == 0)
+                return;
+            for (int i = 1; i < 10; i++)
+                Console.WriteLine("{0} * {1}={2}", x, i, x * i);
+            Console.ReadLine();
+
+        }
+    }
+}
