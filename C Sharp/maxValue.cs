029b325f42d63ed9e9a08cd1c1ca245fd5afb5e7
Balakrishna <iitbalu1993@gmail.com>
2017-07-12 13:22:30 +0530

    To find the Max value code

---
+++ C Sharp/maxValue.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxValue
+{
+    class maxValue
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.Write("Enter value to x:");
+            x = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter value to y:");
+            y = int.Parse(Console.ReadLine());
+
+            if (x > y)
+                Console.WriteLine("X is greater");
+            else
+            {
+                if (x < y)
+                    Console.WriteLine("Y is greater");
+                else
+                    Console.WriteLine("Both are Same");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
