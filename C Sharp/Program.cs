550949b1492ee7ec6bc5e1a4f8c445782b4374f0
Balakrishna <iitbalu1993@gmail.com>
2017-08-30 12:27:02 +0530

    Creating a String Object By Using C#

---
+++ C Sharp/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace CalculatorApplication
+{
+    class NumberManipulator
+    {
+        public int FindMax(int num1, int num2)
+        {
+            /* local variable declaration */
+            int result;
+
+            if (num1 > num2)
+                result = num1;
+            else
+                result = num2;
+
+            return result;
+        }
+    }
+
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            //from string literal and string concatenation
+            string fname, lname;
+            fname = "Balu";
+            lname = "KBK Goud";
+
+            string fullname = fname + lname;
+            Console.WriteLine("Full Name: {0}", fullname);
+
+            //by using string constructor
+            char[] letters = { 'H', 'e', 'l', 'l', 'o' };
+            string greetings = new string(letters);
+            Console.WriteLine("Greetings: {0}", greetings);
+
+            //methods returning string
+            string[] sarray = { "Hello", "From", "Bala", "Krishna" };
+            string message = String.Join(" ", sarray);
+            Console.WriteLine("Message: {0}", message);
+
+            //formatting method to convert a value
+            DateTime waiting = new DateTime(2012, 10, 10, 17, 58, 1);
+            string chat = String.Format("Message sent at {0:t} on {0:D}", waiting);
+            Console.WriteLine("Message: {0}", chat);
+            Console.ReadKey();
+        }
+    }
+}
