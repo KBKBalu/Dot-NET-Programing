4a13c385b3e2e7e67e1258a2040796382eade522
Balakrishna <iitbalu1993@gmail.com>
2017-08-30 12:29:08 +0530

    Creating a String Object By Using C#

--- C Sharp/Program.cs
+++ C Sharp/Program.cs
@@ -1,25 +1,9 @@
 ﻿using System;
 namespace CalculatorApplication
 {
-    class NumberManipulator
+    class Program
     {
-        public int FindMax(int num1, int num2)
-        {
-            /* local variable declaration */
-            int result;
-
-            if (num1 > num2)
-                result = num1;
-            else
-                result = num2;
-
-            return result;
-        }
-    }
-
-    class Test
-    {
-        static void Main(string[] args)
+      static void Main(string[] args)
         {
             //from string literal and string concatenation
             string fname, lname;
